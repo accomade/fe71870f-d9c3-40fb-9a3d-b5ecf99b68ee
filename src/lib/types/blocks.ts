a578d776a34b8ac1fde519acb6fa11ee04500321
Dirk Porsche <dirkporsche78@googlemail.com>
2024-01-15 09:29:20 +0100

    fix: text

--- src/lib/types/blocks.ts
+++ src/lib/types/blocks.ts
@@ -26,8 +26,8 @@ export interface TextBlock {
   content: {
     ref: string
     minHeight?: string
-    minFontSize?: string
-    maxFontSize?: string
+    headerFontSize?: string
+    textFontSize?: string
   }
 }
 
